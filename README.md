84aa9aaa8927623015c85072fcb369a0b66f21c4
HyeokjinKang <coupy@croissant.tech>
2021-01-20 23:39:53 +0900

    더 많은 UI를 지원합니다.

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# better-benedu
+# dark-benedu
 Benedu의 디자인을 크롬 익스텐션을 통해 바꿔줍니다.
-Maintained by 강혁진, 민유, 정한
--- js/global.js
+++ js/global.js
@@ -1,4 +1,3 @@
 document.getElementsByTagName('img')[0].addEventListener('click', () => {
-  console.log('hi');
   window.location.href = 'http://benedu.co.kr/StudentHome';
 });
